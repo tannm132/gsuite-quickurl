c54a44d3a0f45614d82f8c38beb5b2bf4fcf35e5
tannm132 <tannm132@outlook.com>
2019-12-01 15:50:29 +0700

    [Back] Implement qr-code inserter

--- src/qrcode-inserter.ts
+++ src/qrcode-inserter.ts
@@ -1,10 +1,26 @@
-import { IInserter } from './IInserter.interface';
+import * as QRCode from 'qrcode';
+import { IInserter, ITransformed } from './IInserter.interface';
+import { GDocRelatedError } from './custom-exceptions';
 
 class QrCodeInserter implements IInserter {
-  transform(url: string) {
-    return '';
+  async transform(url: string): Promise<any> {
+    try {
+      return await QRCode.toDataURL(url);
+    } catch (err) {
+      throw new Error('Cannot generate QrCode.');
+    }
   }
-  insert(item) {}
+
+  insert(transformedItem: ITransformed): void {
+    const selection = SlidesApp.getActivePresentation().getSelection();
+    const currentPage = selection.getCurrentPage();
+
+    if (currentPage) {
+      currentPage.insertImage(transformedItem as string);
+    } else {
+      throw new GDocRelatedError('Cannot get current page.');
+    }
+  }
 }
 
 export { QrCodeInserter };
